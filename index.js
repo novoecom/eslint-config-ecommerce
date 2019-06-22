6f39bdd3ebaccced715ac40168f486b627440cfe
Udlei Nati <udlei.nati@free2move.me>
2019-06-22 19:37:29 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+module.exports = {
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 9,
+    sourceType: 'module'
+  },
+  env: {
+    node: true,
+    mocha: true,
+    es6: true
+  },
+  rules: {
+    'standard/object-curly-even-spacing': [2, 'either'],
+    'standard/array-bracket-even-spacing': [2, 'either'],
+    'standard/computed-property-even-spacing': [2, 'even'],
+    'standard/no-callback-literal': [2, ['cb', 'callback']],
+    'no-var': 'warn',
+    'prefer-const': 'error',
+    'space-before-function-paren': ['warn', 'never'],
+    'one-var': ['error', 'always'],
+    'no-unused-vars': ['error'],
+    'brace-style': ['error', 'stroustrup'],
+    'prefer-template': ['error']
+  }
+};
